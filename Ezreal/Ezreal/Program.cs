7e9b960a1282d436671dd1655010d13953555260
Mathias <dreamchas3r@outlook.com>
2015-07-14 16:08:21 +0200

    Update Program.cs

--- Ezreal/Ezreal/Program.cs
+++ Ezreal/Ezreal/Program.cs
@@ -20,14 +20,17 @@ namespace Ezreal
 		public static Spell W;
 		public static Spell E;
 		public static Spell R;
+		public static float QMana;
+		public static float WMana;
+		public static float EMana;
 		public static Menu Config;
 
 		private static void Main()
 		{
-			CustomEvents.Game.OnGameLoad += Game_OnGameLoad;
+			CustomEvents.Game.OnGameLoad += OnGameLoad;
 		}
 
-		private static void Game_OnGameLoad(EventArgs args)
+		private static void OnGameLoad(EventArgs args)
 		{
 			if (Player.ChampionName != ChampionName) return;
 			
@@ -36,7 +39,7 @@ namespace Ezreal
 			E = new Spell(SpellSlot.E, 475f);
 			R = new Spell(SpellSlot.R, 3000f);
 
-			Q.SetSkillshot(0.25f, 60f, 2000f, true, SkillshotType.SkillshotLine);
+			Q.SetSkillshot(0.25f, 70f, 2000f, true, SkillshotType.SkillshotLine);
 			W.SetSkillshot(0.25f, 80f, 1600f, false, SkillshotType.SkillshotLine);
 			R.SetSkillshot(1f, 160f, 2000f, false, SkillshotType.SkillshotLine);
 
@@ -45,70 +48,25 @@ namespace Ezreal
 			SpellList.Add(E);
 			SpellList.Add(R);
 
-			Config = new Menu("Ezreal - the Dream Chaser", "Ezreal", true);
-
-			var targetSelectorMenu = new Menu("Target Selector", "Target Selector");
-			TargetSelector.AddToMenu(targetSelectorMenu);
-			Config.AddSubMenu(targetSelectorMenu);
-
-			Config.AddSubMenu(new Menu("Orbwalking", "Orbwalking"));
-			Orbwalker = new Orbwalking.Orbwalker(Config.SubMenu("Orbwalking"));
-
-			Config.AddSubMenu(new Menu("Combo", "Combo"));
-			Config.SubMenu("Combo").AddItem(new MenuItem("UseQCombo", "Use Q in Combo").SetValue(true));
-			Config.SubMenu("Combo").AddItem(new MenuItem("UseWCombo", "Use W in Combo").SetValue(true));
-			Config.SubMenu("Combo").AddItem(new MenuItem("UseECombo", "Use E in Combo").SetValue(new StringList(new[] { "To mouse", "To enemy", "No" })));
-
-			Config.AddSubMenu(new Menu("Harass", "Harass"));
-			Config.SubMenu("Harass").AddItem(new MenuItem("UseQHarass", "Use Q in Harass").SetValue(true));
-			Config.SubMenu("Harass").AddItem(new MenuItem("UseWHarass", "Use W in Harass").SetValue(false));
-			Config.SubMenu("Harass").AddItem(new MenuItem("harassMana", "Min. Mana Percent").SetValue(new Slider(50, 100, 0)));
-
-			Config.SubMenu("Harass").AddSubMenu(new Menu("Auto-Harass", "AutoHarass"));
-			Config.SubMenu("Harass").SubMenu("AutoHarass").AddItem(new MenuItem("harassToggle", "Toggle Key").SetValue(new KeyBind(84, KeyBindType.Toggle)));
-				foreach (var enemyChampion in ObjectManager.Get<Obj_AI_Hero>().Where(enemy => enemy.Team != ObjectManager.Player.Team))
-				{
-					Config.SubMenu("Harass")
-						.SubMenu("AutoHarass")
-						.AddItem(
-							new MenuItem("harass" + enemyChampion.ChampionName, enemyChampion.ChampionName).SetValue(false));
-				}
-
-			Config.AddSubMenu(new Menu("Farm", "Farm"));
-			Config.SubMenu("Farm").AddItem(new MenuItem("farmKey", "Farm Key").SetValue(new KeyBind(67, KeyBindType.Press)));
-			Config.SubMenu("Farm").AddItem(new MenuItem("UseQFarm", "Use Q in Farm").SetValue(true));
-			Config.SubMenu("Farm").AddItem(new MenuItem("farmMana", "Min. Mana Percent").SetValue(new Slider(50, 100, 0)));
-			
-			Config.AddSubMenu(new Menu("LaneClear", "LaneClear"));
-			Config.SubMenu("LaneClear").AddItem(new MenuItem("UseQLane", "Clear with Q").SetValue(true));
-			Config.SubMenu("LaneClear").AddItem(new MenuItem("laneMana", "Min. Mana Percent").SetValue(new Slider(50, 100, 0)));
-
-			Config.AddSubMenu(new Menu("JungleClear", "JungleClear"));
-			Config.SubMenu("JungleClear").AddItem(new MenuItem("UseQJungle", "Clear with Q").SetValue(true));
-			Config.SubMenu("JungleClear").AddItem(new MenuItem("jungleMana", "Min. Mana Percent").SetValue(new Slider(50, 100, 0)));
-
-			Config.AddSubMenu(new Menu("Killsteal", "Killsteal"));
-			Config.SubMenu("Killsteal").AddItem(new MenuItem("UseQKillsteal", "Killsteal with Q").SetValue(true));
-			Config.SubMenu("Killsteal").AddItem(new MenuItem("UseWKillsteal", "Killsteal with W").SetValue(true));
-			Config.SubMenu("Killsteal").AddItem(new MenuItem("UseRKillsteal", "Killsteal with R").SetValue(true));
-
-			Config.AddSubMenu(new Menu("Drawing", "Drawing"));
-			Config.SubMenu("Drawing").AddItem(new MenuItem("qDraw", "Draw Q Range").SetValue(true));
-			Config.SubMenu("Drawing").AddItem(new MenuItem("wDraw", "Draw W Range").SetValue(true));
-			Config.SubMenu("Drawing").AddItem(new MenuItem("eDraw", "Draw E Range").SetValue(true));
-			
-			Config.AddSubMenu(new Menu("Misc", "Misc"));
-			Config.SubMenu("Misc").AddItem(new MenuItem("HitChanceChooser", "Hitchance").SetValue(new StringList(new[] { "Low", "Medium", "High", "Very High" })));
-			Config.SubMenu("Misc").AddItem(new MenuItem("SkinChangerTogggle", "Use Skin Changer").SetValue(false));
-			Config.SubMenu("Misc").AddItem(new MenuItem("SkinChangerID", "Skins").SetValue(new StringList(new[] { "Classic", "Nottingham", "Striker", "Frosted", "Explorer", "Pulsefire", "TPA", "Debonair", "Ace of Spades" })));
-			 
-			Config.AddToMainMenu();
-
-			Game.OnUpdate += Game_OnGameUpdate;
-			Drawing.OnDraw += Drawing_OnDraw;
+			SetMenu();
+			SetEvents();
+			SetMana();
 		}
 
-		private static void Drawing_OnDraw(EventArgs args)
+		private static void OnEnemyGapcloser(ActiveGapcloser gapcloser)
+		{
+			var useE = Config.Item("AntiGapcloser").GetValue<bool>();
+
+			if (useE && Player.Mana >= EMana + QMana)
+			{
+				var target = gapcloser.Sender;
+				if (target == null || !target.IsValidTarget(E.Range)) return;
+
+				E.Cast(Player.Position.Extend(Game.CursorPos, E.Range));
+			}
+		}
+
+		private static void OnDraw(EventArgs args)
 		{
 			var drawQ = Config.Item("qDraw").GetValue<bool>();
 			if (drawQ && Q.IsReady())
@@ -123,7 +81,7 @@ namespace Ezreal
 				Render.Circle.DrawCircle(Player.Position, E.Range, Colors.MistyRose);
 		}
 
-		private static void Game_OnGameUpdate(EventArgs args)
+		private static void OnGameUpdate(EventArgs args)
 		{
 			if (Player != null && Player.BaseSkinName != null)
 			{
@@ -155,46 +113,54 @@ namespace Ezreal
 
 		private static void Combo()
 		{
-			var target = TargetSelector.GetTarget(Q.Range, TargetSelector.DamageType.Physical);
-			if (target == null) return;
+			var qTarget = TargetSelector.GetTarget(Q.Range, TargetSelector.DamageType.Physical);
+			var wTarget = TargetSelector.GetTarget(W.Range, TargetSelector.DamageType.Magical);
+			var eTarget = TargetSelector.GetTarget(E.Range + W.Range, TargetSelector.DamageType.Magical);
+
+			if (qTarget == null || wTarget == null || eTarget == null) return;
 
 			var useQ = Config.Item("UseQCombo").GetValue<bool>();
 			var useW = Config.Item("UseWCombo").GetValue<bool>();
 
-			switch (Config.Item("UseECombo").GetValue<StringList>().SelectedIndex)
+			if (Player.Mana >= WMana + QMana)
 			{
-				case 0:
-					if (E.IsReady() && Q.IsReady() || W.IsReady() && Player.Distance(target) <= Q.Range + E.Range)
-						E.Cast(Game.CursorPos);
-					break;
-				case 1:
-					if (E.IsReady() && Q.IsReady() || W.IsReady() && Player.Distance(target) <= Q.Range + E.Range)
-						E.Cast(target.ServerPosition);
-					break;
-				case 2:
-					break;
+				switch (Config.Item("UseECombo").GetValue<StringList>().SelectedIndex)
+				{
+					case 0:
+						if (E.IsReady() && Q.IsReady() || W.IsReady())
+							E.Cast(Game.CursorPos);
+						break;
+					case 1:
+						if (E.IsReady() && Q.IsReady() || W.IsReady())
+							E.Cast(eTarget.ServerPosition);
+						break;
+					case 2:
+						break;
+				}
 			}
 
 			if (useQ && Q.IsReady())
-				Q.CastIfHitchanceEquals(target, HitChanceChooser());
+				Q.CastIfHitchanceEquals(qTarget, HitChanceChooser());
 			if (useW && W.IsReady())
-				W.CastIfHitchanceEquals(target, HitChanceChooser());
+				W.CastIfHitchanceEquals(wTarget, HitChanceChooser());
 		}
 
 		private static void Harass()
 		{
 			if (Config.Item("harassMana").GetValue<Slider>().Value >= (Player.Mana / Player.MaxMana) * 100) return;
 
-			var target = TargetSelector.GetTarget(Q.Range, TargetSelector.DamageType.Physical);
-			if (target == null) return;
+			var qTarget = TargetSelector.GetTarget(Q.Range, TargetSelector.DamageType.Physical);
+			var wTarget = TargetSelector.GetTarget(W.Range, TargetSelector.DamageType.Magical);
+
+			if (qTarget == null || wTarget == null) return;
 
 			var useQ = Config.Item("UseQHarass").GetValue<bool>();
 			var useW = Config.Item("UseWHarass").GetValue<bool>();
 
 			if (useQ && Q.IsReady())
-				Q.CastIfHitchanceEquals(target, HitChanceChooser());
+				Q.CastIfHitchanceEquals(qTarget, HitChanceChooser());
 			if (useW && W.IsReady())
-				W.CastIfHitchanceEquals(target, HitChanceChooser());
+				W.CastIfHitchanceEquals(wTarget, HitChanceChooser());
 		}
 
 		private static void HarassToggle()
@@ -304,6 +270,83 @@ namespace Ezreal
 				default:
 					return HitChance.VeryHigh;
 			}
+		}
+
+		private static void SetMenu()
+		{
+			Config = new Menu("Ezreal - the Dream Chaser", "Ezreal", true);
+
+			var targetSelectorMenu = new Menu("Target Selector", "Target Selector");
+			TargetSelector.AddToMenu(targetSelectorMenu);
+			Config.AddSubMenu(targetSelectorMenu);
+
+			Config.AddSubMenu(new Menu("Orbwalking", "Orbwalking"));
+			Orbwalker = new Orbwalking.Orbwalker(Config.SubMenu("Orbwalking"));
+
+			Config.AddSubMenu(new Menu("Combo", "Combo"));
+			Config.SubMenu("Combo").AddItem(new MenuItem("UseQCombo", "Use Q in Combo").SetValue(true));
+			Config.SubMenu("Combo").AddItem(new MenuItem("UseWCombo", "Use W in Combo").SetValue(true));
+			Config.SubMenu("Combo").AddItem(new MenuItem("UseECombo", "Use E in Combo").SetValue(new StringList(new[] { "To mouse", "To enemy", "No" })));
+
+			Config.AddSubMenu(new Menu("Harass", "Harass"));
+			Config.SubMenu("Harass").AddItem(new MenuItem("UseQHarass", "Use Q in Harass").SetValue(true));
+			Config.SubMenu("Harass").AddItem(new MenuItem("UseWHarass", "Use W in Harass").SetValue(false));
+			Config.SubMenu("Harass").AddItem(new MenuItem("harassMana", "Min. Mana Percent").SetValue(new Slider(50, 100, 0)));
+
+			Config.SubMenu("Harass").AddSubMenu(new Menu("Auto-Harass", "AutoHarass"));
+			Config.SubMenu("Harass").SubMenu("AutoHarass").AddItem(new MenuItem("harassToggle", "Toggle Key").SetValue(new KeyBind(84, KeyBindType.Toggle)));
+			foreach (var enemyChampion in ObjectManager.Get<Obj_AI_Hero>().Where(enemy => enemy.Team != ObjectManager.Player.Team))
+			{
+				Config.SubMenu("Harass")
+					.SubMenu("AutoHarass")
+					.AddItem(
+						new MenuItem("harass" + enemyChampion.ChampionName, enemyChampion.ChampionName).SetValue(false));
+			}
+
+			Config.AddSubMenu(new Menu("Farm", "Farm"));
+			Config.SubMenu("Farm").AddItem(new MenuItem("farmKey", "Farm Key").SetValue(new KeyBind(67, KeyBindType.Press)));
+			Config.SubMenu("Farm").AddItem(new MenuItem("UseQFarm", "Use Q in Farm").SetValue(true));
+			Config.SubMenu("Farm").AddItem(new MenuItem("farmMana", "Min. Mana Percent").SetValue(new Slider(50, 100, 0)));
+
+			Config.AddSubMenu(new Menu("LaneClear", "LaneClear"));
+			Config.SubMenu("LaneClear").AddItem(new MenuItem("UseQLane", "Clear with Q").SetValue(true));
+			Config.SubMenu("LaneClear").AddItem(new MenuItem("laneMana", "Min. Mana Percent").SetValue(new Slider(50, 100, 0)));
+
+			Config.AddSubMenu(new Menu("JungleClear", "JungleClear"));
+			Config.SubMenu("JungleClear").AddItem(new MenuItem("UseQJungle", "Clear with Q").SetValue(true));
+			Config.SubMenu("JungleClear").AddItem(new MenuItem("jungleMana", "Min. Mana Percent").SetValue(new Slider(50, 100, 0)));
+
+			Config.AddSubMenu(new Menu("Killsteal", "Killsteal"));
+			Config.SubMenu("Killsteal").AddItem(new MenuItem("UseQKillsteal", "Killsteal with Q").SetValue(true));
+			Config.SubMenu("Killsteal").AddItem(new MenuItem("UseWKillsteal", "Killsteal with W").SetValue(true));
+			Config.SubMenu("Killsteal").AddItem(new MenuItem("UseRKillsteal", "Killsteal with R").SetValue(true));
+
+			Config.AddSubMenu(new Menu("Drawing", "Drawing"));
+			Config.SubMenu("Drawing").AddItem(new MenuItem("qDraw", "Draw Q Range").SetValue(true));
+			Config.SubMenu("Drawing").AddItem(new MenuItem("wDraw", "Draw W Range").SetValue(true));
+			Config.SubMenu("Drawing").AddItem(new MenuItem("eDraw", "Draw E Range").SetValue(true));
+
+			Config.AddSubMenu(new Menu("Misc", "Misc"));
+			Config.SubMenu("Misc").AddItem(new MenuItem("AntiGapcloser", "Use E against Gapclosers").SetValue(true));
+			Config.SubMenu("Misc").AddItem(new MenuItem("HitChanceChooser", "Hitchance").SetValue(new StringList(new[] { "Low", "Medium", "High", "Very High" })));
+			Config.SubMenu("Misc").AddItem(new MenuItem("SkinChangerTogggle", "Use Skin Changer").SetValue(false));
+			Config.SubMenu("Misc").AddItem(new MenuItem("SkinChangerID", "Skins").SetValue(new StringList(new[] { "Classic", "Nottingham", "Striker", "Frosted", "Explorer", "Pulsefire", "TPA", "Debonair", "Ace of Spades" })));
+
+			Config.AddToMainMenu();
+		}
+
+		private static void SetEvents()
+		{
+			Game.OnUpdate += OnGameUpdate;
+			Drawing.OnDraw += OnDraw;
+			AntiGapcloser.OnEnemyGapcloser += OnEnemyGapcloser;
+		}
+
+		private static void SetMana()
+		{
+			QMana = Q.Instance.ManaCost;
+			WMana = W.Instance.ManaCost;
+			EMana = E.Instance.ManaCost;
 		}
 	}
 }
